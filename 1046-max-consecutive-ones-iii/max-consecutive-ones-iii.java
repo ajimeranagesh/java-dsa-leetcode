7fbf2b0fe7f2cc1e5d3c981d9fb1a04db6b98424
Nagesh Ajmeera <ajimeranagesh@gmail.com>
2026-07-24 21:09:04 +0530

    Time: 2527 ms (5.00%) | Memory: 51.7 MB (98.21%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -26,34 +26,3 @@ class Solution {
     }
 } 
 
-/*
-    class Solution {
-
-    public int longestOnes(int[] nums, int k) {
-
-        int maxLength = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-
-            int zeros = 0;
-
-            for (int j = i; j < nums.length; j++) {
-
-                if (nums[j] == 0) {
-                    zeros++;
-                }
-
-                if (zeros <= k) {
-
-                    maxLength = Math.max(maxLength, j - i + 1);
-
-                } else {
-
-                    break;
-                }
-            }
-        }
-
-        return maxLength;
-    }
-} */
